84811f232a8bdf0c966dca164b2f3340472b9b0e
NhatLinh20 <nhatlinh.kg20@gmail.com>
2026-05-30 09:56:14 +0700

    à

--- src/lib/latex-parser/index.ts
+++ src/lib/latex-parser/index.ts
@@ -1,7 +1,7 @@
 // src/lib/latex-parser/index.ts
 // Module chính - export tất cả parser functions
 
-export { parseTexFile, extractAndValidateBlocks } from './file-parser'
+export { parseTexFile, extractAndValidateBlocks, formatImportReport } from './file-parser'
 export { parseQuestion } from './question-parser'
 export { parseCategoryCode } from './category-parser'
 export { detectQuestionType, detectCorrectAnswer } from './answer-parser'
